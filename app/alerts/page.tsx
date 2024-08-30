c847d55954e710234a5d10d8d4ed8e93034cc516
Sooryadev T J <140253623+sooryadev007@users.noreply.github.com>
2024-08-30 18:47:55 +0530

    created landing page,alertpage,safety page

---
+++ app/alerts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const alerts = () => {
+  return <div>alerts</div>;
+};
+
+export default alerts;
---
+++ app/safety/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const safety = () => {
+  return <div>safety</div>;
+};
+
+export default safety;
